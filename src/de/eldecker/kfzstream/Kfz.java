cbb83c9b94f6f4ad5e3470c7e76e2be5a91a5145
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-12-05 17:44:55 +0100

    Update Kfz.java

--- src/de/eldecker/kfzstream/Kfz.java
+++ src/de/eldecker/kfzstream/Kfz.java
@@ -6,7 +6,8 @@ package de.eldecker.kfzstream;
  * 
  * @param kennzeichen KFZ-Kennzeichen, z.B. "KA-X 1234"
  * 
- * @param ps Leistung des Motors in Pferdestärken (1PS entspricht ca. 735,5 Watt)
+ * @param ps Leistung des Motors in Pferdestärken 
+ *           (1PS entspricht ca. 735,5 Watt)
  */
 public record Kfz( String kennzeichen, 
 		           int    ps ) {}
