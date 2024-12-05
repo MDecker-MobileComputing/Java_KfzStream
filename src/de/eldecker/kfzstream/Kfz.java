761860d78ca1d23772b6b1a5daaa4534e4fd6a7b
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-12-05 18:09:02 +0100

    Tab2Space fuer beide Java-Dateien

--- src/de/eldecker/kfzstream/Kfz.java
+++ src/de/eldecker/kfzstream/Kfz.java
@@ -3,11 +3,11 @@ package de.eldecker.kfzstream;
 
 /**
  * Record-Klasse für Kraftfahrzeuge.
- * 
+ *
  * @param kennzeichen KFZ-Kennzeichen, z.B. "KA-X 1234"
- * 
- * @param ps Leistung des Motors in Pferdestärken 
+ *
+ * @param ps Leistung des Motors in Pferdestärken
  *           (1PS entspricht ca. 735,5 Watt)
  */
-public record Kfz( String kennzeichen, 
-		           int    ps ) {}
+public record Kfz( String kennzeichen,
+                   int    ps ) {}
--- src/de/eldecker/kfzstream/Main.java
+++ src/de/eldecker/kfzstream/Main.java
@@ -6,48 +6,48 @@ import java.util.stream.Stream;
 
 
 /**
- * Hauptklasse. 
+ * Hauptklasse.
  */
 public class Main {
 
-	/** Implementierung für Terminal-Operation von {@link Kfz}-Stream. */
-	private static final Consumer<Kfz> KFZ_VERBRAUCHER = kfz -> { System.out.println( kfz ); };
-	
-	/** Objekt zum Vergleich von zwei {@link Kfz}-Objekten, wird zum Sortieren benötigt. */ 
-	private static final Comparator<Kfz> KFZ_VERGLEICHER = 
-			(kfz1,kfz2) -> kfz1.kennzeichen().compareTo( kfz2.kennzeichen() );
-	
-	
-	/**
-	 * Methode erzeugt Stream mit Kfz-Objekten. 
-	 * 
-	 * @return Stream mit {@link Kfz}-Objekten mit Kennzeichen und PS.
-	 */
-	private static Stream<Kfz> getKfzStream() {
-		
-		return Stream.of(
-							new Kfz( "KA-Z 988" , 220 ),
-							new Kfz( "KA-AB 901", 80  ),
-							new Kfz( "HD-Y 1234", 140 ),
-							new Kfz( "KA-T 1972", 205 ),
-							new Kfz( "HH-DB 444", 230 ),
-							new Kfz( "KA-B 4321", 330 )
-				        );
-	}
-	
-	
-	/**
-	 * Einstiegsmethode.
-	 * 
-	 * @param args Wird nicht ausgewertet.
-	 */
-	public static void main( String[] args ) {
+    /** Implementierung für Terminal-Operation von {@link Kfz}-Stream. */
+    private static final Consumer<Kfz> KFZ_VERBRAUCHER = kfz -> { System.out.println( kfz ); };
 
-		System.out.println();
-		
-		final Stream<Kfz> kfzStream = getKfzStream();
-		
-		kfzStream.forEach( KFZ_VERBRAUCHER );		
-	}
-	
+    /** Objekt zum Vergleich von zwei {@link Kfz}-Objekten, wird zum Sortieren benötigt. */
+    private static final Comparator<Kfz> KFZ_VERGLEICHER =
+            (kfz1,kfz2) -> kfz1.kennzeichen().compareTo( kfz2.kennzeichen() );
+
+
+    /**
+     * Methode erzeugt Stream mit Kfz-Objekten.
+     *
+     * @return Stream mit {@link Kfz}-Objekten mit Kennzeichen und PS.
+     */
+    private static Stream<Kfz> getKfzStream() {
+
+        return Stream.of(
+                            new Kfz( "KA-Z 988" , 220 ),
+                            new Kfz( "KA-AB 901", 80  ),
+                            new Kfz( "HD-Y 1234", 140 ),
+                            new Kfz( "KA-T 1972", 205 ),
+                            new Kfz( "HH-DB 444", 230 ),
+                            new Kfz( "KA-B 4321", 330 )
+                        );
+    }
+
+
+    /**
+     * Einstiegsmethode.
+     *
+     * @param args Wird nicht ausgewertet.
+     */
+    public static void main( String[] args ) {
+
+        System.out.println();
+
+        final Stream<Kfz> kfzStream = getKfzStream();
+
+        kfzStream.forEach( KFZ_VERBRAUCHER );
+    }
+
 }
